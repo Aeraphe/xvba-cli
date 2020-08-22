b2044edcafa1b019903a3120bc3bf4be6101eda9
aeraph <alberto.aeraph@gmail.com>
2020-08-22 17:58:08 -0300

    Create init command

--- index.js
+++ index.js
@@ -3,14 +3,42 @@
 
 const program = require('commander');
 const package = require('./package.json');
+const fs = require("fs");
 
 program.version(package.version);
 
 program
-    .command('add [todo]')
+    .command('add [xvba]')
     .description('Adiciona um to-do')
     .action((todo) => {
         console.log(todo);
     });
+
+program
+    .command('init [xvba]')
+    .description('Create a new config files')
+    .action(() => {
+
+        try {
+
+            fs.writeFile("./config.json", JSON.stringify({
+                app_name: "XVBA",
+                description: "",
+                author: "",
+                email: "",
+                create_ate: "",
+                excel_file: "index.xlsb",
+                vba_folder: "vba-files",
+                xvba_packages: [],
+                xvba_dev_packages: [],
+            }, null, 4), () => {
+
+            })
+        } catch (error) {
+            console.log(error)
+        }
+
+    });
+
 
 program.parse(process.argv);
