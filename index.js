1598ccc023ec164ceb4ebcfc5541af0119a961ed
aeraph <alberto.aeraph@gmail.com>
2020-09-16 03:16:16 -0300

    add new command create

--- index.js
+++ index.js
@@ -6,10 +6,17 @@ const addXvbaFromGit = require('./actions/add.action');
 const delPackage = require('./actions/del-package.action')
 const buildPackage = require('./actions/build-package.action')
 const listPackages = require('./actions/list-packages.action')
+const createPackageScaffold = require('./actions/create-package.action');
 
 program.command('add [xvba]').description('Add XVB Package fro GitHub').action((url) => { addXvbaFromGit(url); });
 
-program.command('instal [xvba]').description('Instal Xvba Package from Xvba Repository').action((package) => { buildPackage(package) })
+program.command('install [xvba]').description('Instal Xvba Package from Xvba Repository').action((package) => { buildPackage(package) })
+
+program.command('i [xvba]').description('Instal Xvba Package from Xvba Repository').action((package) => { buildPackage(package) })
+
+
+program.command('create [xvba]').description('Create Xvba Package scaffold on xvba_modules').action((package) => { createPackageScaffold(package) })
+
 
 program.command('build [xvba]').description('Create Xvba Package').action((package) => { buildPackage(package) })
 
