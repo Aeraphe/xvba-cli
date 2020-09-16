d45f015431ab8647e51af352276ed8fa3bb4c315
aeraph <alberto.aeraph@gmail.com>
2020-09-16 01:12:35 -0300

    Fix  path formt with path.join

--- gitClone.js
+++ gitClone.js
@@ -1,17 +1,18 @@
 const { exec } = require('child_process');
 const rootPath = process.cwd();
+const path = require('path');
 
 const gitClone = (folder, params, callBack) => {
 
     let [user, package] = params;
     let repoFolder = "";
     if (folder) {
-        repoFolder = rootPath + "/" + folder + "/" + package;
+        repoFolder = path.join(rootPath, folder, package);
     } else {
-        repoFolder = rootPath + "/xvba_modules/" + package;
+        repoFolder = path.join(rootPath, "xvba_modules", package);
     }
 
-    exec(" git clone https://github.com/" + user + "/" + package + ".git " + "\"" + repoFolder +"\"",
+    exec(" git clone https://github.com/" + user + "/" + package + ".git " + "\"" + repoFolder + "\"",
         (error, stdout, stderr) => {
             if (error) {
                 callBack(false);
@@ -22,7 +23,7 @@ const gitClone = (folder, params, callBack) => {
                 callBack(stderr.includes("Cloning"));
                 return;
             }
-            
+
 
         })
 }
