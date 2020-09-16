985c82997a011d5fee813a327764d26ebd024143
aeraph <alberto.aeraph@gmail.com>
2020-09-16 02:49:07 -0300

    Add changelog file

---
+++ changelog.m
@@ -0,0 +1,16 @@
+# Changelog
+
+## Command Line Interface for XVBA VSCode extension (Under Construction)
+
+- This package provides a way to add VBA packages in Excel with XVBA VSCode extension
+- Add third packages to your project with command line interface
+
+
+
+## [1.0.0b3] - 2020-09-16
+### Added
+- New Command Build XVBA Package "npm xvba build [package]"
+- New Command Create XVBA Package "npm xvba create [package]"
+
+### Fixed
+- Refectory all code
