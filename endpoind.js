bf1f3158e87c00a59307ca3d5fde7d1cd1d8d74d
aeraph <alberto.aeraph@gmail.com>
2020-09-17 19:05:24 -0300

    Create endpoint manager

---
+++ endpoind.js
@@ -0,0 +1,13 @@
+//const baseUrl = 'http://localhost:5001/xvba-691e3/us-central1';
+const baseUrl = 'https://us-central1-xvba-691e3.cloudfunctions.net';
+const apiVersion = '/api/v1/';
+const url = baseUrl + apiVersion;
+/**
+ * Global Api Endpoints Manager
+ */
+export const packages = {
+   
+    getFile: url + 'packages/download/'
+}
+
+
