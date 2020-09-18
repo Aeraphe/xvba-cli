2bd7de632833f7be24769bc60abfda791e5fb3af
aeraph <alberto.aeraph@gmail.com>
2020-09-18 00:58:05 -0300

    Add extract zip, read config & make package

--- actions/install.action.js
+++ actions/install.action.js
@@ -1,35 +1,60 @@
 const { default: Axios } = require('axios')
-
 const Fs = require('fs');
 const urls = require('../helper/endpoints')
 const os = require('os')
 const path = require('path')
-const zlib = require('zlib');
+const extract = require('extract-zip')
+const rootPath = process.cwd();
+const { promisify } = require('util');
+const readFileAsync = promisify(Fs.readFile);
+const mkdirAsync = promisify(Fs.mkdir);
+const statAsync = promisify(Fs.stat);
+const checkRootFolder =  require('../helper/check-root-folder.helper')
+
 
 
 module.exports = async (package) => {
     try {
+
+        if (!checkRootFolder()) { return; };
+        const twirlTimer = (function () {
+            let P = ["\\", "|", "/", "-"];
+            let x = 0;
+            return setInterval(function () {
+                process.stdout.write("\r" + P[x++]);
+                x &= 3;
+            }, 50);
+        })();
+ 
         const url = urls.packages.getFile + package;
+        //Get temp dir 
         const tempDir = os.tmpdir()
-        const filePath = path.resolve(tempDir, 'xvba_modules', 'package.xvba')
-        await new Promise(
-            (resolve) => {
-                Fs.mkdir(path.join(tempDir, 'xvba_modules'), () => {
-                    resolve()
-                })
-            }
-        )
+        const filePath = path.resolve(tempDir, 'xvba-modules', 'package.xvba')
+        const xvbaModulesTempFolder = path.join(tempDir, 'xvba-modules')
+        //Check temp folder exists
+        const stats = await statAsync(xvbaModulesTempFolder)
+        if (!stats.isDirectory) {
+            await mkdirAsync(xvbaModulesTempFolder)
+        }
+        //Read file from Http request
         const writer = Fs.createWriteStream(filePath);
-        const response = await Axios({ url, method: 'Get', responseType: 'stream' })
-        let unzip = zlib.createUnzip()
-        response.data.pipe(writer)
-       
-        await new Promise(
-            (resolve, reject) => {
-                writer.on('finish', resolve);
-                writer.on('close', reject);
-            }
-        )
+        const response = await Axios({ url, method: 'Get', responseType: 'stream' });
+        response.data.pipe(writer);
+        //Stream Complete
+        await new Promise((resolve, reject) => { writer.on('finish', resolve); writer.on('close', reject); });
+        //Package temp folder
+        const packageTempfolder = path.join(xvbaModulesTempFolder, 'package');
+        //Extract filet to temp folder
+        await extract(path.join(xvbaModulesTempFolder, 'package.xvba'), { dir: packageTempfolder });
+        //Reade config file
+        const config = await readFileAsync(path.join(packageTempfolder, 'xvba.package.json')).then(data => JSON.parse(data));
+        const packageFolder = path.join(rootPath, 'xvba_modules', config.package);
+        //Save package to xvba_modules folder
+        await extract(path.join(xvbaModulesTempFolder, 'package.xvba'), { dir: packageFolder });
+
+        console.log('Package ' + config.package + ' has Successfully installed ')
+        console.log('\nThanks for using XVBA')
+        twirlTimer.unref()
 
     } catch (error) {
         console.log(error)
