0beddcbf645d9f91baf86b41b445ee3be99034db
aeraph <alberto.aeraph@gmail.com>
2020-09-18 01:55:14 -0300

    Change del action command to name to uninstall

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const program = require('commander');
 
 //Actions
 
-const delPackage = require('./actions/del-package.action')
+const uninstallPackage = require('./actions/uninstall-package.action')
 const buildPackage = require('./actions/build-package.action')
 const listPackages = require('./actions/list-packages.action')
 const createPackageScaffold = require('./actions/create-package.action');
@@ -13,14 +13,15 @@ const installPackage = require('./actions/install.action')
 //Install
 program.command('install [xvba]').description('Instal Xvba Package from Xvba Repository').action((package) => { installPackage(package) })
 program.command('i [xvba]').description('Instal Xvba Package from Xvba Repository').action((package) => { installPackage(package) })
-
+//Uninstall
+program.command('uninstall [xvba]').description('Delete Package').action((package) => { uninstallPackage(package) });
 
 program.command('create [xvba]').description('Create Xvba Package scaffold on xvba_modules').action((package) => { createPackageScaffold(package) })
 
 
 program.command('build [xvba]').description('Create Xvba Package').action((package) => { buildPackage(package) })
 
-program.command('del [xvba]').description('Delete Package').action((pacId) => { delPackage(pacId) });
+
 
 program.command('init [xvba]').description('Create a new config files').action();
 
