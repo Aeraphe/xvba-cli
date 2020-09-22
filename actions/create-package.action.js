a10abbc8c34523473f56f5e122c52bc756ae4b98
aeraph <alberto.aeraph@gmail.com>
2020-09-22 20:16:40 -0300

    Add create of INDEX_CLASS file

--- actions/create-package.action.js
+++ actions/create-package.action.js
@@ -28,7 +28,15 @@ module.exports = async function createPackageScaffold(package) {
         )
         await new Promise(
             (resolve, reject) => {
-                fs.writeFile(path.join(xvbaModulesPath, 'CHANGELOG.md'), CHANGE_LOG(package), (error => {
+                fs.writeFile(path.join(xvbaModulesPath, `${package}_xvba.cls`), INDEX_CLASS(package), (error => {
+                    error ? reject(error) : resolve(true)
+                }))
+            }
+        )
+
+        await new Promise(
+            (resolve, reject) => {
+                fs.writeFile(path.join(xvbaModulesPath, `CHANGELOG.md`), CHANGE_LOG(package), (error => {
                     error ? reject(error) : resolve(true)
                 }))
             }
@@ -67,19 +75,71 @@ handleCheckFolder = (packagePath) => {
 }
 
 
+function jsUcfirst(string) 
+{
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+/**
+ * 
+ * @param {String} package 
+ */
+const INDEX_CLASS = (package) =>{
+    const name = jsUcfirst(package);
+    return `VERSION 1.0 CLASS
+    BEGIN
+      MultiUse = -1  'True
+    END
+    Attribute VB_Name = "${name}"
+    Attribute VB_GlobalNameSpace = False
+    Attribute VB_Creatable = False
+    Attribute VB_PredeclaredId = False
+    Attribute VB_Exposed = False
+
+'namespace=xvba_modules\\${package}
+
+
+
+'Example of Package Create by XVBA-CLI
+
+'Class Param Example
+Private pName As String
+
+
+'Main method example
+Private Sub main()
+
+  MsgBox "message",,"title"
+
+End Sub
+
+
+'Example of accessor method Get
+Public Property Get Name() As String
+  Name = pName
+End Property
+
+
+'Example of accessor method Let
+Public Property Let Name(Value as String) As String
+  pName =  Value
+End Property
+
+    `
+}
+
 const README = (package) => {
     return `# ${package} (Example of Readme File)
     
-    ## Command ...
-       
-    - This package provides a way to ....`
+## Command ...
+ - This package provides a way to ....`
 }
 
 
 const XVBA_PACKAGE = (package) => {
     return {
         "package": package,
-        "version": "1.0",
+        "version": "1.0.0",
         "author": "",
         "email": "",
         "license": "ISC",
@@ -97,20 +157,18 @@ const XVBA_PACKAGE = (package) => {
 
 const CHANGE_LOG = (package) => {
     return `# Changelog
+## ${package} (Under Construction)
 
-    ## ${package} (Under Construction)
+- This package provides a way to add VBA packages in Excel with XVBA VSCode extension
+- Add third packages to your project with command line interface
     
-    - This package provides a way to add VBA packages in Excel with XVBA VSCode extension
-    - Add third packages to your project with command line interface
-    
-    
-    
-    ## [1.0.0b3] - 2020-09-16
-    ### Added
-    - New Command Build XVBA Package "npm xvba build [package]"
-    - New Command Create XVBA Package "npm xvba create [package]"
-    
-    ### Fixed
-    - Refectory all code
-    `
+## [1.0.0b3] - 2020-09-16
+### Added
+ - New Command Build XVBA Package "npm xvba build [package]"
+ - New Command Create XVBA Package "npm xvba create [package]"
+
+### Fixed
+ - Refectory all code
+ 
+ `
 }
