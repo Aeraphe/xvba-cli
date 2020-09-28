d9c46837960f1d83faf78d0f5bede2b5146b68d3
aeraph <alberto.aeraph@gmail.com>
2020-09-28 18:26:08 -0300

    Rename all Commands descriptions

--- index.js
+++ index.js
@@ -12,24 +12,24 @@ const addVbaFile = require('./actions/add-file.action')
 
 
 //Install
-program.command('install').description('Instal Xvba Package from Xvba Repository').action((package) => { installPackage(package) })
-program.command('i').description('Instal Xvba Package from Xvba Repository').action((package) => { installPackage(package) })
+program.command('install').description('xvba install [package] - Instal Xvba Package from Xvba Repository').action((package) => { installPackage(package) })
+program.command('i').description('xvba i [package] - Instal Xvba Package from Xvba Repository').action((package) => { installPackage(package) })
 //Uninstall
-program.command('uninstall').description('Delete Package').action((package) => { uninstallPackage(package) });
+program.command('uninstall').description('xvba uninstall [package] -  Uninstall Package').action((package) => { uninstallPackage(package) });
 
-program.command('create').description('Create Xvba Package scaffold on xvba_modules').action((package) => { createPackageScaffold(package) })
+program.command('create').description('xvba create [package] - Create Xvba Package scaffold on xvba_modules').action((package) => { createPackageScaffold(package) })
 
 
-program.command('build').description('Create Xvba Package').action((package) => { buildPackage(package) })
+program.command('build').description('xvba build [package] - Build Xvba Package file ').action((package) => { buildPackage(package) })
 
 //Add new vba files
-program.command('add [m,c]').description('Create Xvba Package').action( async (filePath) => { addVbaFile(filePath,program) })
+program.command('add [m,c]').description('xvba add -c [sub-folder/file] or xvba add -m [sub-folder/file] - Create Vba file type module bas or class cls').action( async (filePath) => { addVbaFile(filePath,program) })
 
 
 
-program.command('init').description('Create a new config files').action();
+program.command('init').description('xvba init - Create a new config files').action();
 
-program.command('ls').description('List all Packages').action(() => { listPackages() });
+program.command('ls').description('xvba ls - List all Packages installed').action(() => { listPackages() });
 
 program
   .option('-p, --package', 'Create a Package')
