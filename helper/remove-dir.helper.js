208d923964c9e24f9b7f4192c7e5ca311abce3b7
Aeraphe <alberto.aeraph@gmail.com>
2020-10-27 19:41:24 -0300

    Refactory remove package with rimraf

--- helper/remove-dir.helper.js
+++ helper/remove-dir.helper.js
@@ -1,22 +1,9 @@
 const fs = require("fs");
-
+var rimraf = require("rimraf");
 
 module.exports = removeDir = function (path) {
     if (fs.existsSync(path)) {
-        const files = fs.readdirSync(path)
-
-        if (files.length > 0) {
-            files.forEach(function (filename) {
-                if (fs.statSync(path + "/" + filename).isDirectory()) {
-                    removeDir(path + "/" + filename)
-                } else {
-                    fs.unlinkSync(path + "/" + filename)
-                }
-            })
-            fs.rmdirSync(path)
-        } else {
-            fs.rmdirSync(path)
-        }
+        rimraf(path, function () { console.log("Thanks for using XVBA-CLI"); });
     } else {
         console.log("Directory path not found.")
     }
